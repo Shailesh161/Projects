4a2bec76e8adeaae20fbee783505f800b312ce5a
Shailesh Gatkul <112454440+Shailesh161@users.noreply.github.com>
2024-03-08 09:57:48 +0530

    Update pathfinder.py

--- Shortest_Path_Finder-main/pathfinder.py
+++ Shortest_Path_Finder-main/pathfinder.py
@@ -37,6 +37,7 @@ class BFS:
 			a = self.queue[0]
 			self.queue.remove(a)
 			for i in range(self.n):
+				#If the condition is true, a becomes the parent of node i , and node i is marked as visited, and it is added to queue.
 				if self.matrix[a][i]==1 and self.visited[i]==0:
 					self.parent[i]= a
 					self.visited[i]= 1
@@ -44,12 +45,12 @@ class BFS:
 					if i==n:
 						self.queue=[]
 						break
-		if i!=n:
+		if i!=n:    #If the current node i is the destination node n, then queue is cleared, and the loop is terminated.
 			return				
 		self.traversed =[]
 		p=int(self.parent[n])
 		self.traversed.append(p)
-		while(p!=m):
+		while(p!=m):   #Once the destination node n is found, then code starts moving backward,checking each node's parent until it reaches the starting node m 
 			p = int(self.parent[p])
 			if p==m:
 				break
@@ -58,33 +59,58 @@ class BFS:
 		
 class Body:
 	def __init__(self):
-		self.width = 1000
+		#These lines set the width and height of the window or screen to 1000 pixels and 700 pixels, respectively.
+		self.width = 1000   
 		self.height = 700
+  
+		#creates a turtle graphics screen object using the Screen()
 		self.screen =tl.Screen()
+  
+		#sets dimensions of screen using previously defined width and height.
 		self.screen.setup(self.width, self.height)
 		self.screen.title("Finding the shortest path between two points.")
 		self.screen.bgcolor("white")
+  
+		#current position of the turtle or object on the screen.
 		self.x=0
 		self.y=0
+  
+		# controls whether the main loop of the program should continue running.
 		self.run =True
-		self.dx = 48
-		self.dy =20
-		self.step = 20
+	
+		#self.dx and self.dy represent the change in position (delta x and delta y) for each step taken by the turtle.
+		self.dx = 48  #48 units horizontally
+		self.dy =20	  #20 units vertically
+		self.step = 20 #distance the turtle will move in each step.
+		
+  		#By setting self.cx to -470 and self.cy to 110, the turtle's starting position is adjusted to a specific location on the screen.
 		self.cy = 110
-		self.cx = -470		
+		self.cx = -470
+
+		#List to store points that are prohibited or restricted from traversing 
 		self.prohibited=[]
+  
+		#List to store the shortest path found between two points
 		self.rs =[]
-		self.write()
-		self.grid()
+		self.write() #used to write information on screen
+		self.grid()	 #to create grid
 		self.allow = True
 		#self.main()
 		self.hs =[]
+  
+		# to store the index of the starting point & end point in the program.
 		self.start_pt = 0
 		self.end_pt = 959
+
+		#to indicate whether the starting point has been set or not
 		self.start = True
 		self.end =False
+  
+		#to indicate whether hurdles or obstacles are present or not
 		self.hurdles =False		
-		self.listen_mouse_clicks()	
+		self.listen_mouse_clicks()
+		
+  		# to keep the program running and responsive, allowing it to handle user interactions and update the screen as necessary.
 		self.screen.mainloop()	
 		
 
@@ -101,35 +127,38 @@ class Body:
 		except:
 			tmsg.showinfo("Not Found","No path found between between the start and the end points")		
 
-		'''		
-		#self.draw_point(959)
-		#while self.run:
 		
-		#	self.screen.update()
-		'''
 	def draw_point(self ,n):
+		#creates a new turtle object named self.block
 		self.block = tl.Turtle()
+		#pen up from the drawing surface, so no drawing will occur 
 		self.block.penup()
-		self.block.shape("circle")#square
-		self.block.color("black")
-		self.block.speed(0)
+		self.block.shape("circle")#square to change shape (It is shape of drawing points during traversal)
+		self.block.color("black") # The color of points is set to the black.
+		self.block.speed(0)       #speed is set to 0 to move turtle instantly
 		self.block.goto(self.cx+20*(n%self.dx),self.cy-20*(n//self.dx))	
-		self.rs.append(self.block)
+		self.rs.append(self.block) #stores references to all the turtle objects representing points drawn on the screen.
 
+	#To initialize start point
+	#After starting the drawing we cannot add end point and hurdles in program during ongoing pathfinding process hence end & hurdles set to false 
 	def draw_start(self):
-		self.start=True
-		self.end = False
-		self.hurdles=False
+		self.start=True  #flag to indicate that user is expected to select a starting point
+		self.end = False  
+		self.hurdles=False 
 
+	#To initialize the end point.
 	def draw_end(self):
 		self.start=False
-		self.end = True
+		self.end = True #flag to indicate that user is expected to select a ending point
 		self.hurdles=False
 
+	#Used to Draw hurdles.
 	def draw_hurdles(self):
 		self.start=False
 		self.end = False
-		self.hurdles=True
+		self.hurdles=True#flag to indicate that user is expected to select a hurdles
+  
+  #instructions are displayed in an organized way by making them easy for the user to read and follow.
 	def write(self):
 		self.pen = tl.Turtle()
 		self.pen.penup()
@@ -137,7 +166,7 @@ class Body:
 		self.pen.speed(0)
 		self.pen.color("black")
 		self.pen.goto(-480,300)
-		self.pen.write("Shortest Path Finder",False,align="left",font=("arial",20,"bold"))	
+		self.pen.write("Shortest Path Finder",False,align="left",font=("arial",20,"bold"))	#False indicates text should not be underlined
 		self.pen.goto(-480,270)
 		self.pen.write("Instructions:",False,align="left",font=("arial",15,"bold"))	
 		self.pen.goto(-480,250)
@@ -153,56 +182,76 @@ class Body:
 		self.pen.goto(-480,150)
 		self.pen.write("f) Press 'o' in the keyboard to clear the grid.",False,align="left",font=("arial",10,"bold"))
 
-	def draw_squares(self,x,y):
-		xcor =None
-		ycor =None
-		for i in range(self.dx):
-			if x<(self.cx+20*i)+10 and x>(self.cx+20*i)-10:
-				xcor= self.cx+20*i
-				break
-		for j in range(self.dy):
-			if y<(self.cy-20*j)+10 and y>(self.cy-20*j)-10:
-				ycor= self.cy-20*j
-				break
-		if self.hurdles:		
-			if xcor and ycor:	
-				loc =j*self.dx + i
-				self.prohibited.append(loc)	
-				self.t = tl.Turtle()
-				self.t.penup()
-				self.t.shape("circle")
-				self.t.color("pink")
-				self.t.speed(-1)
-				self.t.goto(xcor,ycor)
-				self.hs.append(self.t)
-		if self.start:
-			if xcor and ycor:	
-				self.start_pt =j*self.dx + i
-				try:
-					self.st.goto(xcor,ycor)
-				except:	
-					self.st = tl.Turtle()
-					self.st.penup()
-					self.st.shape("square")
-					self.st.color("green")
-					self.st.speed(-1)
-					self.st.goto(xcor,ycor)
-		if self.end:
-			if xcor and ycor:	
-				self.end_pt = j*self.dx + i
-				try:
-					self.et.goto(xcor,ycor)
-				except:	
-					self.et = tl.Turtle()
-					self.et.penup()
-					self.et.shape("square")
-					self.et.color("blue")
-					self.et.speed(-1)
-					self.et.goto(xcor,ycor)	
+		#draw_squares takes the mouse click coordinates (x and y) and finds the corresponding grid cell coordinates (xcor and ycor).
+		def draw_squares(self,x,y):
+			xcor =None
+			ycor =None
+
+			#loops over the possible x-coordinates on the grid and checks if the mouse click falls within a specific range or not
+			for i in range(self.dx):
+				if x<(self.cx+20*i)+10 and x>(self.cx+20*i)-10:
+					xcor= self.cx+20*i  #If the mouse click is within the range, it sets the xcor to the center of that grid cell
+					break
+					
+			#loops over the possible y-coordinates on the grid and checks if the mouse click falls within a specific range 
+			for j in range(self.dy):
+				if y<(self.cy-20*j)+10 and y>(self.cy-20*j)-10:
+					ycor= self.cy-20*j #If the mouse click is within the range, it sets the ycor to the center of that grid cell
+					break
+ 
+			#If the self.hurdles flag is set and valid grid coordinates are found:
+			if self.hurdles:		
+				if xcor and ycor:	
+					loc =j*self.dx + i   #calculates location of obstacle. 
+					self.prohibited.append(loc)	 #Adds the obstacle location to the list of prohibited locations
+					self.t = tl.Turtle()
+					self.t.penup()
+					self.t.shape("circle")
+					self.t.color("pink")
+					self.t.speed(-1)  #slowest speed 
+					self.t.goto(xcor,ycor)
+					self.hs.append(self.t)
+     
+			#If the self.start flag is set and valid grid coordinates are found
+			if self.start:
+				if xcor and ycor:	
+					self.start_pt =j*self.dx + i   # calculates the location of the starting point 
+					try:
+						self.st.goto(xcor,ycor)
+					
+     				#If no turtle object exists, it creates one, sets its appearance to green square
+					except:	
+						self.st = tl.Turtle()
+						self.st.penup()
+						self.st.shape("square")
+						self.st.color("green")
+						self.st.speed(-1)
+						self.st.goto(xcor,ycor)
+      
+			#If the self.end flag is set and valid grid coordinates are found:
+			if self.end:
+				if xcor and ycor:	
+					self.end_pt = j*self.dx + i   #calculates the location of the ending point
+					try:
+						self.et.goto(xcor,ycor)
+      
+					#If no turtle object exists, it creates one, sets it to blue square.
+					except:	
+						self.et = tl.Turtle()
+						self.et.penup()
+						self.et.shape("square")
+						self.et.color("blue")
+						self.et.speed(-1)
+						self.et.goto(xcor,ycor)	
+	
+ 	
 	def erase_all(self):
 		try:
-			self.et.goto(1000,1000)
+			#These lines move the turtle objects starting point (self.st) and the ending point (self.et) to coordinates (1000, 1000)
 			self.st.goto(1000,1000)
+			self.et.goto(1000,1000)
+			
+   			#self.hs, which presumably contains obstacles or hurdles.
 			for i in self.hs:
 				i.goto(1000,1000)
 			self.hs = []
@@ -211,12 +260,14 @@ class Body:
 				i.goto(1000,1000)
 			self.rs = []			
 		except:
-			pass	
+			pass
+
+	
 	def random_hurdles(self):
-		if self.allow:
-			for i in range(100):
-				loc = random.randint(0,959)
-				self.prohibited.append(loc)	
+		if self.allow:    						#used to ensure that obstacles are generated only when allowed
+			for i in range(100):   				#This loop iterates 100 times, generating 100 random obstacles.
+				loc = random.randint(0,959)		#random locations
+				self.prohibited.append(loc)	    #This adds the randomly generated location to the list prohibited
 				self.t = tl.Turtle()
 				self.t.penup()
 				self.t.shape("square")
@@ -228,12 +279,16 @@ class Body:
 		self.allow =True	
 
 		
-	def listen_mouse_clicks(self):	
+	def listen_mouse_clicks(self):
+		#Without this line, the screen would not respond to any user input.
 		self.screen.listen()
+  
+		#When the screen detects a mouse click, it calls the draw_squares method, which handles drawing objects at particular cell
 		self.screen.onclick(self.draw_squares)
+  
 		self.screen.onkeypress(self.main,"space")
 		self.screen.onkeypress(self.draw_start,"s")
-		self.screen.onkeypress(self.draw_end,"e")
+		self.screen.onkeypress(self.draw_end,"e")  # when e is pressed calls draw_end method
 		self.screen.onkeypress(self.draw_hurdles,"h")
 		self.screen.onkeypress(self.erase_all,"o")
 		self.screen.onkeypress(self.random_hurdles,"r")
@@ -241,30 +296,23 @@ class Body:
 		
 
 	def grid(self):
-
+		#creates self.g object with black colour 
 		self.g = tl.Turtle()
 		self.g.color("black") 
 		self.g.hideturtle()
 		self.g.speed(-1)
-		for i in range(21):
+		for i in range(21): #Grid contains 21 horizontal lines
 			self.g.penup()
 			self.g.goto(-480,+120-(20*i))
 			self.g.pendown()
 			self.g.forward(480*2)	
-		for i in range(49):
+		for i in range(49): #Grid contains 49 vertical lines.
 			self.g.penup()
 			self.g.setheading(-90)
 			self.g.goto(-480+20*i,+120)
 			self.g.pendown()
 			self.g.forward(400)		
-			'''
-		self.block = tl.Turtle()
-		self.block.penup()
-		self.block.shape("square")
-		self.block.color("black")
-		self.block.speed(0)
-		self.block.goto(self.cx+20*(self.dx-1),self.cy-20*(self.dy-1))	
-		'''
+		
 
 if __name__=="__main__":
 	b =Body()
